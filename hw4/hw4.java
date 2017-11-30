48b9e829454d6195ac2e3f7ada6f97a406df8710
nemimccarter <nemimccarter@gmail.com>
2017-11-30 03:52:52 -0800

    Finish required parts

--- hw4/hw4.java
+++ hw4/hw4.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 import java.sql.*;
 
 public class hw4 {
-
+    
     public static void main( String args[] ) {
         Scanner in = new Scanner(System.in);
         int userChoice;
@@ -11,7 +11,18 @@ public class hw4 {
                query = "";
         boolean runProgram = true;
         ResultSet results = null; 
-        SqlFunction sqlObject = new SqlFunction();
+        Statement statement = null;
+        Connection connection = null;
+        
+        try {
+            Class.forName("org.sqlite.JDBC");
+            connection = DriverManager.getConnection("jdbc:sqlite:chinook.db");
+            connection.setAutoCommit(false);
+            statement = connection.createStatement();
+        } catch ( Exception e ) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            System.exit(0);
+        }
 
         while (runProgram) {
             results = null;
@@ -42,9 +53,11 @@ public class hw4 {
                             "(SELECT ArtistId " +
                             "FROM Artist " +
                             "WHERE Name = " + "'" +  inputString + "'" + ")";
-                    results = sqlObject.sqlFunction(query);
 
+                    
                     try {
+                        results = statement.executeQuery(query);
+
                         while (results.next()) {
                             isEmpty = false;
                             String title = results.getString("Title");
@@ -71,8 +84,8 @@ public class hw4 {
                             "WHERE Track.AlbumId = A.AlbumId " +
                             "AND IL.InvoiceId = I.InvoiceId " +
                             "AND I.CustomerId = " + '"' + inputString + '"';
-                    results = sqlObject.sqlFunction(query);
                     try {
+                        results = statement.executeQuery(query);
                         while (results.next()) {
                             isEmpty = false;
                             String trackName = results.getString("Name"),
@@ -88,11 +101,35 @@ public class hw4 {
                         System.err.println( e.getClass().getName() + ": " + e.getMessage() );
                         System.exit(0);
                     }
-
-
                     break;
 
                 case (4) :
+                    System.out.println("Enter a track ID: ");
+                    inputString = in.nextLine();
+                    query = "SELECT UnitPrice FROM Track WHERE TrackID = " + '"' + inputString + '"';
+                    try {
+                        results = statement.executeQuery(query);
+                        String price = results.getString("UnitPrice");
+                        System.out.println("Current Price: $" + price + "\nEnter new price: ");
+                        float newPrice = in.nextFloat();
+                        query = "UPDATE Track set UnitPrice = " + '"' + newPrice + '"' + 
+                                " WHERE TrackId = " + '"' + inputString + '"';
+                       statement.executeUpdate(query);
+                        connection.commit();
+                    } catch ( Exception e ) {
+                        System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+                        System.exit(0);
+                    }
+                    System.out.println("Record updated.");
+                    query = "SELECT UnitPrice FROM Track WHERE TrackId = " + '"' + inputString + '"';
+                    try {
+                        results = statement.executeQuery(query);
+                        String price = results.getString("UnitPrice");
+                        System.out.println("New price: $" + price);
+                    } catch ( Exception e ) {
+                        System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+                        System.exit(0);
+                    }
 
                     break;
 
