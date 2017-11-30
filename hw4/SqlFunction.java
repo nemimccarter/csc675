edc5aca6520c2ba50fee7264d0a4dc675c27969f
nemimccarter <nemimccarter@gmail.com>
2017-11-30 02:45:13 -0800

    Complete part 2 of hw4

--- hw4/SqlFunction.java
+++ hw4/SqlFunction.java
@@ -1,30 +1,25 @@
 import java.sql.*;
 
 public class SqlFunction {
-    void sqlFunction(String query) {
+    ResultSet sqlFunction(String query) {
         Connection c = null;
         boolean isEmpty = true;
         Statement statement = null;
+        ResultSet resultSet = null;
         try {
             Class.forName("org.sqlite.JDBC");
             c = DriverManager.getConnection("jdbc:sqlite:chinook.db");
             c.setAutoCommit(false);
 
             statement = c.createStatement();
-            ResultSet resultSet = statement.executeQuery(query);
+            resultSet = statement.executeQuery(query);
             
-            while (resultSet.next()) {
-                isEmpty = false;
-                String Album = resultSet.getString("Title");
-                System.out.println(Album);
-            }
-            if (isEmpty) {
-                System.out.println("Returned 0 results");
-            }
-
+            
         } catch ( Exception e ) {
             System.err.println( e.getClass().getName() + ": " + e.getMessage() );
             System.exit(0);
         }
+
+        return resultSet;
     }
 }
--- hw4/hw4.java
+++ hw4/hw4.java
@@ -1,16 +1,24 @@
 import java.util.Scanner;
+import java.sql.*;
 
 public class hw4 {
 
     public static void main( String args[] ) {
         Scanner in = new Scanner(System.in);
         int userChoice;
-        String inputString,
-               query;
+        boolean isEmpty = true;
+        String inputString = "",
+               query = "";
         boolean runProgram = true;
+        ResultSet results = null; 
         SqlFunction sqlObject = new SqlFunction();
 
         while (runProgram) {
+            results = null;
+            isEmpty = true;
+            query = "";
+            inputString = "";
+
             System.out.println("Select an option:\n" +
                 "0. Exit\n" +
                 "1. Obtain Album title by Artist name\n" +
@@ -34,11 +42,53 @@ public class hw4 {
                             "(SELECT ArtistId " +
                             "FROM Artist " +
                             "WHERE Name = " + "'" +  inputString + "'" + ")";
-                    sqlObject.sqlFunction(query);
-                    
+                    results = sqlObject.sqlFunction(query);
+
+                    try {
+                        while (results.next()) {
+                            isEmpty = false;
+                            String title = results.getString("Title");
+                            System.out.println(title);
+                        }
+            
+                        if (isEmpty) {
+                            System.out.println("Returned 0 results");
+                        }
+                    } catch ( Exception e ) {
+                        System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+                        System.exit(0);
+                    }
+
+
                     break;
 
                 case (3) :
+                    System.out.println("Enter customer ID: ");
+                    inputString = in.nextLine();
+
+                    query = "SELECT Track.Name, A.Title, IL.Quantity, I.InvoiceDate " +
+                            "FROM Track, Album A, InvoiceLine IL, Invoice I " +
+                            "WHERE Track.AlbumId = A.AlbumId " +
+                            "AND IL.InvoiceId = I.InvoiceId " +
+                            "AND I.CustomerId = " + '"' + inputString + '"';
+                    results = sqlObject.sqlFunction(query);
+                    try {
+                        while (results.next()) {
+                            isEmpty = false;
+                            String trackName = results.getString("Name"),
+                                   albumTitle = results.getString("Title"),
+                                   quantity = results.getString("Quantity"),
+                                   date = results.getString("InvoiceDate"); 
+                            System.out.println(trackName + " | " + albumTitle + " | " + quantity + " | " + date);
+                        }
+                        if (isEmpty) {
+                            System.out.println("Returned 0 results");
+                        }
+                    } catch ( Exception e ) {
+                        System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+                        System.exit(0);
+                    }
+
 
                     break;
 
